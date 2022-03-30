13e085f9c4b922fa5d7b0331a5d835dcc0b978f5
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-03-30 17:51:15 -0300

    Organizando Repositório C

---
+++ ADS_Estacio--Estrutura_de_Dados/CodeBlocks/Testando/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+// Exemplo de Estrutura:
+struct Aluno
+{
+    char nome[100];
+    int matricula;
+    int idade;
+    char cidade[50];
+    char sexo;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    struct Aluno a1;
+    strcpy(a1.nome, "Dhionathan L Jobim");
+    a1.idade = 20;
+    strcpy(a1.cidade,  "Rio Novo");
+    a1.matricula = 202001237331;
+    a1.sexo = 'M';
+    printf("Dados do Aluno1: \n");
+
+    printf("Nome: ");
+    puts(a1.nome);
+
+    printf("Cidade: ");
+    puts(a1.cidade);
+
+    printf("Matricula: %d", a1.matricula);
+
+    return 0;
+}
